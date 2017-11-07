5f08ba48e34e4fd4ce88a2aa33346d0d16e2a4ff
Artur Romasiuk <artur.romasiuk@gmail.com>
2017-11-07 23:20:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/artur/survey/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.artur.survey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.loadUrl("http://free-ig-video-views.com/app/");
+    }
+}
